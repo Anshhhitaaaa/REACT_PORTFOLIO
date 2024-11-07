01224c2af7fc7b697b03cc84526742f44847babd
Anshita Agrawal <agrawal.anshita07@gmail.com>
2024-11-07 21:55:10 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,92 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I’m an enthusiastic first-year student at Indira Gandhi Delhi Technical University for Women, currently pursuing a B.Tech in Computer Science. My journey in tech has just begun, but I'm already excited about the possibilities ahead! With a foundational knowledge in HTML, CSS, JavaScript, Python, and SQL, I’m actively building skills in web development and programming, eager to explore more advanced technologies as I grow.
+
+
+My Skills and Interests
+
+Languages: HTML, CSS, JavaScript, Python, SQL
+
+Interests: Web Development, Problem Solving, Data Structures
+
+Currently Learning: Git, React, or algorithms`;
+
+export const ABOUT_TEXT = `I’m passionate about using technology to solve real-world problems and am driven by a curiosity to learn and experiment. As I progress in my studies, I look forward to taking on exciting projects, collaborating with like-minded individuals, and contributing to meaningful work. I’m especially interested in fields like web development, data science, and AI, and I’m eager to explore how these technologies can impact lives positively. Each course and project has introduced me to new concepts and techniques, sparking my enthusiasm for learning even more. Outside of academics, I enjoy working on small personal projects to put theory into practice, whether it's creating simple web apps, solving coding challenges, or experimenting with basic data analysis.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Web Developer",
+    company: "IEEE IGDTUW",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2024 - Present",
+    role: "Mentee",
+    company: "Google Developer Group On Campus IGDTUW",
+    description: `Led a team in developing and maintaining web applications using Flutter, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2024 - Present",
+    role: "Event Management",
+    company: "Synergy IGDTUW",
+    description: `Through this role, members of the Event Management team enhance skills in leadership, organization, and problem-solving, building a strong foundation for managing complex projects. Event management in Synergy is not only about creating events but also about fostering community and school spirit through sports, inspiring involvement, and celebrating athletic achievements`,
+    technologies: ["Teamwork", "Planning", "Promotion", "Team Collaboration", "Communication"],
+  },
+  {
+    year: "2024 - present",
+    role: "Web Developer",
+    company: "Synergy IGDTUW",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "html", "javascript", "CSS"],
+  },
+  {
+    year: "2024 - Present",
+    role: "Social Media Designer",
+    company: "Green Sphere IGDTUW",
+    description: `A Social Media Designer uses tools like Canva, Photoshop, and Figma for graphics, and Premiere Rush for video creation. For photo editing, Lightroom and Snapseed are useful. Social media management apps like Buffer and Hootsuite schedule posts, while Crello and Slack help with team collaboration.`,
+    technologies: ["Canva", "Figma", "Picsart", "Crello", "Adobe Photoshop"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Hauz Khas, Green Park, SDA, DDA Flats, South-West Delhi",
+  phoneNo: "+91 9315298434 ",
+  email: "agrawal.anshita07@gmail.com",
+};
